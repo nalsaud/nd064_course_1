6dd0792c3117bb6424a159f563b407e8cda5f259
nalsaud <nalsaud@ejada.com>
2025-05-13 00:24:44 +0300

    Add Project with my changes

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,14 +1,24 @@
+import logging
+from datetime import datetime
 import sqlite3
-
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+#Logging Configration
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)s:app:%(asctime)s, %(message)s',
+    datefmt='%m/%d/%Y, %H:%M:%S'
+)
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
     return connection
+
 
 # Function to get a post using its ID
 def get_post(post_id):
@@ -20,6 +30,8 @@ def get_post(post_id):
 
 # Define the Flask application
 app = Flask(__name__)
+#for db
+db_connection_count = 0
 app.config['SECRET_KEY'] = 'your secret key'
 
 # Define the main route of the web application 
@@ -36,14 +48,33 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      logging.info("Article with ID %s not found!", post_id)
       return render_template('404.html'), 404
     else:
+      logging.info('Article "%s" retrieved!', post['title'])
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    logging.info("About Us page retrieved")
     return render_template('about.html')
+# Define health EndPoint
+@app.route('/healthz')
+def healthz():
+    return jsonify(result="OK - healthy"), 200
+# Define metrics EndPoint
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    post_count = len(posts)
+    connection.close()
+
+    return jsonify(
+        db_connection_count=db_connection_count,
+        post_count=post_count
+    ), 200
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
@@ -60,7 +91,7 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            logging.info('New article created: "%s"', title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
